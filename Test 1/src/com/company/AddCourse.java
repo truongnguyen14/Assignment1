61bafd5a54d292b391c9500e703e15b4a723a624
truongnguyen14 <80660271+truongnguyen14@users.noreply.github.com>
2022-03-30 19:35:15 +0700

    Delete AddCourse.java

--- Test 1/src/com/company/AddCourse.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-
-public class AddCourse {
-    public static Course AddCourse(String courseid, String name, int credit){
-        return new Course(courseid, name, credit);
-    }
-}
