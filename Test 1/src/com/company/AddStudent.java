8f0b889fe47bcc0de8ef64422a8a80352f16b03e
truongnguyen14 <80660271+truongnguyen14@users.noreply.github.com>
2022-03-30 19:35:24 +0700

    Delete AddStudent.java

--- Test 1/src/com/company/AddStudent.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-
-public class AddStudent {
-    public static  Student AddStudent(String studentid, String name, String dob){
-        return new Student(studentid, name, dob);
-    }
-}
